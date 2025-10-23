03725c5ec231a3760a424d46d9f2d09d81d75129
YacineMARZOUK <yacinemarzou@gmail.com>
2025-10-23 11:10:39 +0100

    add the Vehicle classe

---
+++ src/main/java/com/logistique/gestiontournees/entity/Vehicle.java
@@ -0,0 +1,34 @@
+package com.logistique.gestiontournees.entity;
+
+
+
+
+import com.logistique.gestiontournees.entity.enumeration.VehicleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(nullable = false, unique = true)
+    private String licensePlate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VehicleType vehicleType;
+
+    @Column(nullable = false)
+    private double maxWeight;
+
+    @Column(nullable = false)
+    private double maxVolume;
+}
