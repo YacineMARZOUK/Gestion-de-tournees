73dbf923d888cc30e76ca0637c4ef48dfe75742d
YacineMARZOUK <yacinemarzou@gmail.com>
2025-10-23 14:34:03 +0100

    add the delivery entity & deliverySatatus

---
+++ src/main/java/com/logistique/gestiontournees/entity/Delivery.java
@@ -0,0 +1,40 @@
+package com.logistique.gestiontournees.entity;
+
+import com.logistique.gestiontournees.entity.enumeration.DeliveryStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "Delivery")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private double longitude;
+
+    @Column(nullable = false)
+    private double weight; // en kg
+
+    @Column(nullable = false)
+    private double volume; // en m³
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeliveryStatus status;
+
+    @Column(nullable = true) // Optionnel
+    private String timeSlot;
+}
