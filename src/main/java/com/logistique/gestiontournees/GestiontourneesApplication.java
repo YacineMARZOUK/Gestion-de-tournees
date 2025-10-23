7c6f7bb3609b775e1054d1cec8308dbc24f6f421
YacineMARZOUK <yacinemarzou@gmail.com>
2025-10-23 10:03:25 +0100

    initializ the project

---
+++ src/main/java/com/logistique/gestiontournees/GestiontourneesApplication.java
@@ -0,0 +1,13 @@
+package com.logistique.gestiontournees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiontourneesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestiontourneesApplication.class, args);
+    }
+
+}
