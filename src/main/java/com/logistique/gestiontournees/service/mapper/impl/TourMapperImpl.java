58b76e9f174201c1f538349197e9c32096c239b2
YacineMARZOUK <yacinemarzou@gmail.com>
2025-10-24 17:23:03 +0100

    fixe the Tour mapper to use the builder

--- src/main/java/com/logistique/gestiontournees/service/mapper/impl/TourMapperImpl.java
+++ src/main/java/com/logistique/gestiontournees/service/mapper/impl/TourMapperImpl.java
@@ -1,5 +1,8 @@
 package com.logistique.gestiontournees.service.mapper.impl;
 
+import com.logistique.gestiontournees.dto.TourDTO;
+import com.logistique.gestiontournees.entity.Tour;
+import com.logistique.gestiontournees.entity.Vehicle;
 import com.logistique.gestiontournees.service.mapper.DeliveryMapper;
 import com.logistique.gestiontournees.service.mapper.TourMapper;
 import com.logistique.gestiontournees.service.mapper.VehicleMapper;
@@ -7,9 +10,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class TourMapperImpl implements TourMapper, ApplicationContextAware {
+import java.util.stream.Collectors;
 
-    private ApplicationContext applicationContext;
+public class TourMapperImpl implements TourMapper {
+
     private final VehicleMapper vehicleMapper;
     private final DeliveryMapper deliveryMapper;
 
@@ -20,8 +24,36 @@ public class TourMapperImpl implements TourMapper, ApplicationContextAware {
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+    public Tour toEntity(TourDTO dto){
+        if(dto == null){ return null;}
+
+        Vehicle vehicle = null;
+
+        if(dto.getVehicleId()!= null){
+            vehicle = Vehicle.builder().id(dto.getVehicleId()).build();
+        }
+
+        return Tour.builder()
+                .id(dto.getId())
+                .tourDate(dto.getTourDate())
+                .vehicle(vehicle)
+                .build();
+    }
+
+    @Override
+    public TourDTO toDto(Tour entity){
+        if(entity == null){ return null;}
+
+        return TourDTO.builder()
+                .id(entity.getId())
+                .tourDate(entity.getTourDate())
+                .vehicleId((entity.getVehicle() != null) ? entity.getVehicle().getId() : null)
+                .vehicle((entity.getVehicle() != null) ? vehicleMapper.toDto(entity.getVehicle()) : null)
+                .deliveries((entity.getDeliveries() != null) ?
+                        entity.getDeliveries().stream()
+                                .map(deliveryMapper::toDto)
+                                .collect(Collectors.toList()) : null)
+                .build();
     }
 
 
